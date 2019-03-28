51582dc92849d73271c12f179d730a332268e673
Nicholas Miron <nicholasmiron@gmail.com>
2019-03-28 15:58:10 -0500

    properly loading content on to page from get request

--- client/src/prodDesc.jsx
+++ client/src/prodDesc.jsx
@@ -5,28 +5,45 @@ class ProdDesc extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPoduct: {}
+      currentProduct: {
+        "_id": "5c9d29a61596fd8ae892cfb8",
+        "name": "Maple Syrup",
+        "price": 10.25,
+        "bulletOne": "Delicious Canadian Nectar",
+        "bulletTwo": "Great with cereal",
+        "bulletThree": "Getnly milked from the sturdiest of trees",
+        "sellerName": "Quebec",
+        "description": "This isn't aunt gemimas crap. The greatest thing since forever ago.",
+        "productID": 1,
+        "__v": 0
+    }
     }
   }
 
   componentDidMount() {
-    this.getProduct(1);
+    this.getProduct(99);
   }
 
   getProduct(productId) {
-    axios.get(`localhost:65535/api/products/id?id={productId}`)
-    .then(result => {
-      this.setState({currentPoduct: result}, console.log(this.state.currentPoduct));
-    })
-    .catch(err => {
-      console.log(err);
-    })
+    axios.get(`/api/products/id?id=${productId}`)
+    .then(result => this.setState({currentProduct: result.data}))
+    .catch(err => console.log(err))
   }
 
 
   render() {
     return (
-      <h1>Hello There</h1>
+      <>
+        <h1>{this.state.currentProduct.name}</h1>
+        <h3>{this.state.currentProduct.sellerName}</h3>
+        <h3>${this.state.currentProduct.price}</h3>
+        <ul>
+          <li>{this.state.currentProduct.bulletOne}</li>
+          <li>{this.state.currentProduct.bulletTwo}</li>
+          <li>{this.state.currentProduct.bulletThree}</li>
+        </ul>
+        <p>{this.state.currentProduct.description}</p>
+      </>
     )
   }
 }
