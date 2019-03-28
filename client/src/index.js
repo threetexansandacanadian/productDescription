d498a1385b5547b75d3675594b18cd0d78ffa420
Nicholas Miron <nicholasmiron@gmail.com>
2019-03-28 13:09:04 -0500

    Loading barebones react compenent

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProdDesc from './prodDesc.jsx';
+
+
+
+ReactDOM.render(<ProdDesc />, document.getElementById('app'));
---
+++ client/src/prodDesc.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ProdDesc extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <h1>Hello There</h1>
+    )
+  }
+}
+
+export default ProdDesc
