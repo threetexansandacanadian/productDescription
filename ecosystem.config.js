ab4ba6af4464839d4444e854319570749577ef51
Nicholas Miron <nicholasmiron@gmail.com>
2019-03-30 15:31:48 -0500

    added ecostystem file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'productDescription',
+    script: './index.js',
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-191-109-41.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/product-description.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/threetexansandacanadian/productDescription.git',
+      path: '/home/ubuntu/productDescription',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
+    },
+  },
+};
