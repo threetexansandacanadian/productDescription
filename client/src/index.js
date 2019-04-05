459f0f5a68e08511ddc4bc25de23ba5af33b7f1a
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-05 17:32:17 -0500

    Changed a bunch of stuff to get it to work with a proxy

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import ProdDesc from './prodDesc.jsx';
 
-ReactDOM.render(<ProdDesc />, document.getElementById('app'));
+window.ProdDesc = ProdDesc;
