82216da85ecb77ddd1fbd50c43bafdc92d491b85
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-09 14:53:47 -0500

    wip8

--- client/src/prodDesc.js
+++ client/src/prodDesc.js
@@ -24,7 +24,7 @@ export default class ProdDesc extends Component {
   }
 
   getProductById(productId) {
-    axios.get(`http://localhost:3000/api/products/id?id=${productId}`)
+    axios.get(`/api/products/id?id=${productId}`)
     .then(result => this.setState({currentProduct: result.data}))
     .catch(err => console.log(err))
   }
