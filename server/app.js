18abe65e91e1ff3aedfdff74ee8bb9ecbf9be54d
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-09 14:01:50 -0500

    trying stuff

--- server/app.js
+++ server/app.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // app.use(express.json({ urlencoded: true }));
 app.use(express.static('dist'));
