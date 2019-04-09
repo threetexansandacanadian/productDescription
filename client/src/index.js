f9183ae522f6b4474e5284861311aef91d798bbe
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-09 14:48:45 -0500

    wip5

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,7 @@
 import ProdDesc from './prodDesc.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-window.ProdDesc = ProdDesc;
+// window.ProdDesc = ProdDesc;
+
+ReactDOM.render(<ProdDesc />, document.getElementById('app'))
