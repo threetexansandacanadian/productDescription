eefe97f076ad731bf7e62c3f0e2574c057de01af
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-09 19:45:16 -0500

    Changed some stuff around

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,5 @@ import ProdDesc from './prodDesc.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// window.ProdDesc = ProdDesc;
+ReactDOM.render(<ProdDesc />, document.getElementById('prodDesc'));
 
-ReactDOM.render(<ProdDesc />, document.getElementById('app'))
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/threetexansandacanadian/productDescription.git',
       path: '/home/ubuntu/productDescription',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
+      'post-deploy': 'npm install && npm run build-one && pm2 startOrRestart ecosystem.config.js',
     },
   },
 };
